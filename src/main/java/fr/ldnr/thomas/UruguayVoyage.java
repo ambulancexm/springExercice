3f4bbd5be2b10e91cdeb91f9ed53ea9118f3e9e4
thomas <bereziat.th@gmail.com>
2020-01-08 14:17:04 +0100

    lookup method avec ajout de classe abstraite

--- src/main/java/fr/ldnr/thomas/UruguayVoyage.java
+++ src/main/java/fr/ldnr/thomas/UruguayVoyage.java
@@ -1,19 +1,21 @@
 package fr.ldnr.thomas;
 
-public class UruguayVoyage {
+public abstract class UruguayVoyage {
 
 	public void affiche() {
 		UruguayService[] urs = new UruguayService[3];
-		urs[0] = new UruguayService();
+		urs[0] = getService();
 		urs[0].setDestination("Montevideo");
-		urs[1] = new UruguayService();
+		urs[1] = getService();
 		urs[1].setDestination("Mercedes");
-		urs[2] = new UruguayService();
+		urs[2] = getService();
 		urs[2].setDestination("Florida");
 		for (UruguayService uruguayService : urs) {
 			System.out.println(uruguayService.getDestination());
 		}
 	}
+	
+	public abstract UruguayService getService();
 	
 	
 
